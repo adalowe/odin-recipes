74325ea2db3aabe2b1c972ff672e0f6c58bcc1f2
adalowe <hecatexandra@gmail.com>
2022-02-23 20:28:51 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function loadHtml(id, filename) {
+    console.log('div id: ${id}, filename: ${filename}');
+
+let xhttp;
+let element = document.getElementById(id);
+let file = filename;
+
+if ( file ){
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4){
+            if(this.status == 200) {element.innerHTML = this.responseText;}
+            if(this.status == 404) {element.innerHTML = "<h1>Page not found.</h1>";}
+        };
+    };
+    /*buraya kdr: what to do when we request data*/ 
+
+    xhttp.open("GET", 'recipes/${file}', true); /*initialize request*/
+  xhttp.send(); /**send request */
+  return;
+} 
+}
+
